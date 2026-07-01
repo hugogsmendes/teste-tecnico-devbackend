7c4b6ccccd42eabc84aabe45a637305c33d97dc8
Hugo Mendes <hugogs.mendes@gmail.com>
2026-06-30 22:14:23 -0300

    refactor: Alterando o script de criar usuario para ele ser executado como modulo

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -1,10 +1,4 @@
-import sys
 import asyncio
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(PROJECT_ROOT))
-
 from src.database.db import SessionLocal
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.users import User
